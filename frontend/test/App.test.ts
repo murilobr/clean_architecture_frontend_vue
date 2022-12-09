23d52cc4a80ecba2136ad77c47903fcd856c73a6
Murilo da Silva <murilobr@gmail.com>
2022-12-08 23:09:12 -0300

    test: empty todo list

--- frontend/test/App.test.ts
+++ frontend/test/App.test.ts
@@ -1,6 +1,12 @@
 import { mount } from "@vue/test-utils";
 import AppVue from "../src/App.vue";
 
+test("Should test an empty todo list", async function () {
+  const wrapper = mount(AppVue, {});
+  expect(wrapper.get(".total").text()).toBe("Total: 0");
+  expect(wrapper.get(".completed").text()).toBe("Completed: 0%");
+});
+
 test("Should test the todo list", async function () {
   const wrapper = mount(AppVue, {});
   await wrapper.get(".todo-description-input").setValue("A");
