2e28f011c0877eb3f6174c8ae55e334cfb07451c
Murilo da Silva <murilobr@gmail.com>
2022-12-08 22:55:55 -0300

    feat: implements input and add button to add new todo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,30 @@
 <script setup lang="ts">
-const todos = [
-  { description: "Create unit tests", done: false },
-  { description: "Create integration tests", done: false },
-  { description: "Create e2e tests", done: true },
-];
+import { reactive } from "vue";
+
+const todos: any = reactive([]);
+const description = "";
+
 function getTotal() {
   return todos.length;
 }
+
 function getCompleted() {
   const total = getTotal();
-  const completed = todos.filter((todo) => todo.done).length;
+  const completed = todos.filter((todo: any) => todo.done).length;
   console.log(completed, total);
   return Math.round((completed / total) * 100);
+}
+
+function addTodo(description: string) {
+  todos.push({ description, done: false });
 }
 </script>
 
 <template>
   <div class="total">Total: {{ getTotal() }}</div>
   <div class="completed">Completed: {{ getCompleted() }}%</div>
+  <input class="todo-description-input" type="text" v-model="description" />
+  <button class="add-todo-button" @click="addTodo(description)">add</button>
 </template>
 
 <style scoped></style>
