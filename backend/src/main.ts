daaea342d8a6ae2e2dcfb21d74c37682c7d43f78
Murilo da Silva <murilobr@gmail.com>
2022-12-09 07:37:55 -0300

    feat: implements backend get todo list

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,13 @@
+import cors from "cors";
 import express from "express";
+
 const app = express();
+app.use(cors());
+
+const todos = [{ description: "My first todo", done: true }];
+
+app.get("/todos", function (req, res) {
+  res.json(todos);
+});
+
 app.listen(3000);
