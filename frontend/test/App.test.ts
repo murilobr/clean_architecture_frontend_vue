4f675397b1f673ad14aca47cf6ba919599e7ad94
Murilo da Silva <murilobr@gmail.com>
2022-12-08 22:55:27 -0300

    test: update test to add new todo to an empty todo list

--- frontend/test/App.test.ts
+++ frontend/test/App.test.ts
@@ -5,6 +5,6 @@ test("Should test the todo list", async function () {
   const wrapper = mount(AppVue, {});
   await wrapper.get(".todo-description-input").setValue("A");
   await wrapper.get(".add-todo-button").trigger("click");
-  expect(wrapper.get(".total").text()).toBe("Total: 3");
-  expect(wrapper.get(".completed").text()).toBe("Completed: 33%");
+  expect(wrapper.get(".total").text()).toBe("Total: 1");
+  expect(wrapper.get(".completed").text()).toBe("Completed: 0%");
 });
