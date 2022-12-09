b6337fd4c5220dc661ed36317f2813a392ca66de
Murilo da Silva <murilobr@gmail.com>
2022-12-08 22:46:53 -0300

    feat: implements total and completed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const todos = [
+  { description: "Create unit tests", done: false },
+  { description: "Create integration tests", done: false },
+  { description: "Create e2e tests", done: true },
+];
+function getTotal() {
+  return todos.length;
+}
+function getCompleted() {
+  const total = getTotal();
+  const completed = todos.filter((todo) => todo.done).length;
+  console.log(completed, total);
+  return Math.round((completed / total) * 100);
+}
+</script>
 
-<template>Sample Clean Architecture Frontend</template>
+<template>
+  <div class="total">Total: {{ getTotal() }}</div>
+  <div class="completed">Completed: {{ getCompleted() }}%</div>
+</template>
 
 <style scoped></style>
