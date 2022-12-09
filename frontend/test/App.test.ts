d3a6575c21bd59f941b51207b7a3efb489b82a69
Murilo da Silva <murilobr@gmail.com>
2022-12-09 07:45:28 -0300

    test: mount with a todo already in done

--- frontend/test/App.test.ts
+++ frontend/test/App.test.ts
@@ -4,7 +4,7 @@ import AppVue from "../src/App.vue";
 test("Should test an empty todo list", async function () {
   const wrapper = mount(AppVue, {});
   expect(wrapper.get(".total").text()).toBe("Total: 1");
-  expect(wrapper.get(".completed").text()).toBe("Completed: 0%");
+  expect(wrapper.get(".completed").text()).toBe("Completed: 100%");
 });
 
 test("Should test the todo list", async function () {
@@ -12,18 +12,18 @@ test("Should test the todo list", async function () {
   await wrapper.get(".todo-description-input").setValue("A");
   await wrapper.get(".add-todo-button").trigger("click");
   expect(wrapper.get(".total").text()).toBe("Total: 2");
-  expect(wrapper.get(".completed").text()).toBe("Completed: 0%");
-  expect(wrapper.findAll(".todo-description").at(0)?.text()).toBe("A");
-  expect(wrapper.findAll(".todo-done").at(0)?.text()).toBe("false");
+  expect(wrapper.get(".completed").text()).toBe("Completed: 50%");
+  expect(wrapper.findAll(".todo-description").at(1)?.text()).toBe("A");
+  expect(wrapper.findAll(".todo-done").at(1)?.text()).toBe("false");
   await wrapper.get(".todo-description-input").setValue("B");
   await wrapper.get(".add-todo-button").trigger("click");
   expect(wrapper.get(".total").text()).toBe("Total: 3");
-  await wrapper.findAll(".todo-toggle-done-button").at(0)?.trigger("click");
-  expect(wrapper.findAll(".todo-done").at(0)?.text()).toBe("true");
+  await wrapper.findAll(".todo-toggle-done-button").at(1)?.trigger("click");
+  expect(wrapper.findAll(".todo-done").at(1)?.text()).toBe("true");
   expect(wrapper.get(".completed").text()).toBe("Completed: 67%");
-  await wrapper.findAll(".todo-delete-button").at(0)?.trigger("click");
+  await wrapper.findAll(".todo-delete-button").at(1)?.trigger("click");
   expect(wrapper.get(".total").text()).toBe("Total: 2");
-  expect(wrapper.get(".completed").text()).toBe("Completed: 0%");
+  expect(wrapper.get(".completed").text()).toBe("Completed: 50%");
 });
 
 test("Should not let insert duplicated todo", async function () {
@@ -33,5 +33,5 @@ test("Should not let insert duplicated todo", async function () {
   await wrapper.get(".todo-description-input").setValue("A");
   await wrapper.get(".add-todo-button").trigger("click");
   expect(wrapper.get(".total").text()).toBe("Total: 2");
-  expect(wrapper.get(".completed").text()).toBe("Completed: 0%");
+  expect(wrapper.get(".completed").text()).toBe("Completed: 50%");
 });
