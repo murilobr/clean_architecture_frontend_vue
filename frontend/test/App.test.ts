c260eccd608291557c9af62f16a9c41ef7739586
Murilo da Silva <murilobr@gmail.com>
2022-12-08 22:36:11 -0300

    test: mount component and print its html content

--- frontend/test/App.test.ts
+++ frontend/test/App.test.ts
@@ -1,1 +1,7 @@
-test("Should test the todo list", function () {});
+import { mount } from "@vue/test-utils";
+import AppVue from "../src/App.vue";
+
+test("Should test the todo list", function () {
+  const wrapper = mount(AppVue, {});
+  console.log(wrapper.html());
+});
