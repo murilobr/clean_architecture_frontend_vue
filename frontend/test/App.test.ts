f1e20384fb7a59a1e4e77312f5661e91e7b95c27
Murilo da Silva <murilobr@gmail.com>
2022-12-08 22:50:44 -0300

    test: add test to input and add todo

--- frontend/test/App.test.ts
+++ frontend/test/App.test.ts
@@ -1,8 +1,10 @@
 import { mount } from "@vue/test-utils";
 import AppVue from "../src/App.vue";
 
-test("Should test the todo list", function () {
+test("Should test the todo list", async function () {
   const wrapper = mount(AppVue, {});
+  await wrapper.get(".todo-description-input").setValue("A");
+  await wrapper.get(".add-todo-button").trigger("click");
   expect(wrapper.get(".total").text()).toBe("Total: 3");
   expect(wrapper.get(".completed").text()).toBe("Completed: 33%");
 });
