8ba23aa00faa991eb7bace5f969de41192e2f3bd
Murilo da Silva <murilobr@gmail.com>
2022-12-11 21:53:48 -0300

    feat: create httpclient and some implementations

---
+++ frontend/src/infra/http/AxiosAdapter.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import HttpClient from "./HttpClient";
+
+export default class AxiosAdapter implements HttpClient {
+  async get(url: string): Promise<any> {
+    const response = await axios.get(url);
+    return response.data;
+  }
+}
---
+++ frontend/src/infra/http/FetchAdapter.ts
@@ -0,0 +1,8 @@
+import HttpClient from "./HttpClient";
+
+export default class FetchAdapter implements HttpClient {
+  async get(url: string): Promise<any> {
+    const response = await fetch(url);
+    return response.json();
+  }
+}
