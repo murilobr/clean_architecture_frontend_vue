344b250f566e89ecb5d2a88c751f028a8e3e1eb7
Murilo da Silva <murilobr@gmail.com>
2022-12-11 21:54:35 -0300

    refactor: instantiate httpclient adapter and add dependency to gateway

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import TodosGatewayHttp from "./infra/gateway/TodosGatewayHttp";
+import FetchAdapter from "./infra/http/FetchAdapter";
 import "./style.css";
 
 const app = createApp(App);
-const todosGateway = new TodosGatewayHttp();
+// const httpClient = new AxiosAdapter();
+const httpClient = new FetchAdapter();
+const baseUrl = "http://localhost:3000";
+const todosGateway = new TodosGatewayHttp(httpClient, baseUrl);
 app.provide("todosGateway", todosGateway);
 app.mount("#app");
