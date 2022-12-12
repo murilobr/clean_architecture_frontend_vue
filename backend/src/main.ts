a4b6c6e6f7c1bfdeb45620238a97cd5d57dae60f
Murilo da Silva <murilobr@gmail.com>
2022-12-11 22:06:27 -0300

    feat: implements backend create new todo

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,17 @@ import express from "express";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const todos = [{ description: "My first todo", done: true }];
 
 app.get("/todos", function (req, res) {
   res.json(todos);
+});
+
+app.post("/todos", function (req, res) {
+  console.log(req.body);
+  todos.push(req.body);
 });
 
 app.listen(3000);
